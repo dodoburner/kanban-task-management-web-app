6a442a00b9e78345a1b3f95ed4413ab99f2ebfef
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-09-24 22:47:37 +0200

    Add typography styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import './styles/index.css';
+import './styles/Typography.css';
 import App from './App';
 import store from './redux/store';
 import reportWebVitals from './reportWebVitals';
