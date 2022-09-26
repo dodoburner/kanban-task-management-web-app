d7ec3a6290f40782ea8cd212f6dee7f3ecc2d20b
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-09-26 15:40:56 +0200

    Fix header dropdown styling

--- src/components/AllBoardsDropdown.js
+++ src/components/AllBoardsDropdown.js
@@ -11,40 +11,44 @@ export default function AllBoardsDropdown({ setOpenDropdown }) {
   const boards = useSelector((state) => state.boards);
 
   return (
-    <div className="dropdown-modal">
-      <h3>ALL BOARDS ({boards.length})</h3>
-      <div className="dropdown-boards">
-        {boards.map((board, index) => {
-          return (
-            <div
-              className={`dropdown-board ${board.isActive ? "board-active" : ""}`}
-              key={index}
-              onClick={() => {
-                dispatch(boardsSlice.actions.setBoardActive({ index }))
-              }}
-            >
-              <img className="filter-white" src={boardIcon} /> {board.name}
-            </div>
-          );
-        })}
-        <div
-          className="dropdown-board dropdown-create-board-btn"
-          onClick={() => {
-            dispatch(modalsSlice.actions.toggleBoardModal());
-            setOpenDropdown((state) => !state);
-          }}
-        >
-          <img className="filter-purple" src={boardIcon} /> + Create New Board
+    <div className="dropdown-container">
+      <div className="dropdown-modal">
+        <h3>ALL BOARDS ({boards.length})</h3>
+        <div className="dropdown-boards">
+          {boards.map((board, index) => {
+            return (
+              <div
+                className={`dropdown-board ${
+                  board.isActive ? "board-active" : ""
+                }`}
+                key={index}
+                onClick={() => {
+                  dispatch(boardsSlice.actions.setBoardActive({ index }));
+                }}
+              >
+                <img className="filter-white" src={boardIcon} /> {board.name}
+              </div>
+            );
+          })}
+          <div
+            className="dropdown-board dropdown-create-board-btn"
+            onClick={() => {
+              dispatch(modalsSlice.actions.toggleBoardModal());
+              setOpenDropdown((state) => !state);
+            }}
+          >
+            <img className="filter-purple" src={boardIcon} /> + Create New Board
+          </div>
         </div>
-      </div>
 
-      <div className="light-toggle">
-        <img src={lightIcon} />
-        <label className="switch">
-          <input type="checkbox" />
-          <span className="slider round"></span>
-        </label>
-        <img src={darkIcon} />
+        <div className="light-toggle">
+          <img src={lightIcon} />
+          <label className="switch">
+            <input type="checkbox" />
+            <span className="slider round"></span>
+          </label>
+          <img src={darkIcon} />
+        </div>
       </div>
     </div>
   );
