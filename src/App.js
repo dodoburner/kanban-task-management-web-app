226e6a94b57f07ea87b10cc288a40d82f0a355b6
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-10-09 15:33:51 +0200

    Fix responsiveness issues

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Header from "./components/Header";
 import Board from "./components/Board";
+import Sidebar from "./components/Sidebar";
 import { useDispatch, useSelector } from "react-redux";
 import boardsSlice from "./redux/boardsSlice";
 import EmptyBoard from "./components/EmptyBoard";
+import { useMediaQuery } from "react-responsive";
 
 function App() {
   const dispatch = useDispatch();
@@ -11,10 +13,11 @@ function App() {
   const activeBoard = boards.find((board) => board.isActive);
   if (!activeBoard && boards.length > 0)
     dispatch(boardsSlice.actions.setBoardActive({ index: 0 }));
-  
+  const isBigScreen = useMediaQuery({ query: "(min-width: 768px)" });
 
   return (
     <div className={`app ${theme}`}>
+      {isBigScreen && <Sidebar />}
       {boards.length > 0 ? (
         <div className="main">
           <Header />
