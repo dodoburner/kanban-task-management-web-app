2e2fc2fbff3fefdc670c2c82d5e449c9706c6021
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-10-11 14:20:27 +0200

    Add new board from the sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Sidebar.css";
 import showSidebarIcon from "../assets/icon-show-sidebar.svg";
 import hideSidebarIcon from "../assets/icon-hide-sidebar.svg";
 import HeaderDropdown from "./HeaderDropdown";
+import AddEditBoardModal from "../modals/AddEditBoardModal";
 
 export default function Sidebar({ isSideBarOpen, setIsSideBarOpen }) {
+  const [isBoardModalOpen, setIsBoardModalOpen] = useState(false);
   const toggleSidebar = () => {
     setIsSideBarOpen((curr) => !curr);
   };
 
   return (
     <div className={`sidebar  ${!isSideBarOpen && "sidebar-closed"}`}>
-      {isSideBarOpen && <HeaderDropdown />}
+      {isSideBarOpen && <HeaderDropdown setIsBoardModalOpen={setIsBoardModalOpen} />}
       <div
         className={`toggle-sidebar-container  ${
           !isSideBarOpen && "toggle-closed"
@@ -24,6 +26,12 @@ export default function Sidebar({ isSideBarOpen, setIsSideBarOpen }) {
         />
         {isSideBarOpen && <p className="heading-M">Hide Sidebar</p>}
       </div>
+      {isBoardModalOpen && (
+        <AddEditBoardModal
+          type="add"
+          setIsBoardModalOpen={setIsBoardModalOpen}
+        />
+      )}
     </div>
   );
 }
