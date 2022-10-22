7b5aaf1038cdd27e868ef77a75af175d211da40f
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-10-22 18:31:24 +0200

    Create add column column

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { useMediaQuery } from "react-responsive";
+import AddEditBoardModal from "../modals/AddEditBoardModal";
 import "../styles/Board.css";
 import Column from "./Column";
 import EmptyBoard from "./EmptyBoard";
@@ -9,6 +10,7 @@ import Sidebar from "./Sidebar";
 export default function Board() {
   const isBigScreen = useMediaQuery({ query: "(min-width: 768px)" });
   const [isSideBarOpen, setIsSideBarOpen] = useState(true);
+  const [isBoardModalOpen, setIsBoardModalOpen] = useState(false);
   const boards = useSelector((state) => state.boards);
   const board = boards.find((board) => board.isActive === true);
   const columns = board.columns;
@@ -25,12 +27,23 @@ export default function Board() {
       )}
 
       {columns.length > 0 ? (
-        columns.map((col, index) => {
-          return <Column key={index} colIndex={index} />;
-        })
+        <>
+          {columns.map((col, index) => {
+            return <Column key={index} colIndex={index} />;
+          })}
+          <div
+            className="add-column-column heading-XL"
+            onClick={() => {
+              setIsBoardModalOpen(true);
+            }}
+          >
+            + New Column
+          </div>
+        </>
       ) : (
         <EmptyBoard type="edit" />
       )}
+      {isBoardModalOpen && <AddEditBoardModal type="edit" setIsBoardModalOpen={setIsBoardModalOpen} />}
     </div>
   );
 }
