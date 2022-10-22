94ad88ea15b52e3aa04748d28c9028035313d5ed
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-10-22 18:44:28 +0200

    Fix eslint error

--- src/components/HeaderDropdown.js
+++ src/components/HeaderDropdown.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import boardIcon from "../assets/icon-board.svg";
 import darkIcon from "../assets/icon-dark-theme.svg";
